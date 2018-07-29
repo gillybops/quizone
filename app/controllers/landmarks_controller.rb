a7fbe2a65a0950c55e0fb8f3ee1b408157aee4c0
gillybops <gillybops@optonline.net>
2018-07-29 07:29:02 +0000

    Added Link To Simple Form to Footer

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,21 +1,49 @@
 class LandmarksController < ApplicationController
   def index
-    @landmark = Landmark.order("RANDOM()").first   
+    @landmarks = Landmark.first   
+  end
+
+  def new
+    @landmark = Landmark.new
   end
 
   def create
     @landmark = Landmark.create(landmark_params)
-    if @landmark.invalid?
-      flash[:error] = '<strong>Could not save</strong> that is not a Jamaican landmark.' 
+    if @landmark.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @landmark = Landmark.find(params[:id])
+  end
+
+  def edit
+    @landmark = Landmark.find(params[:id])
+  end
+
+  def update
+    @landmark = Landmark.find(params[:id])
+
+    @landmark.update_attributes(landmark_params)
+    if @landmark.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @landmark = Landmark.find(params[:id])
+
+    @landmark.destroy
     redirect_to root_path
   end
 
-  def about
-  end
-
   private
-
+  
   def landmark_params
     params.require(:landmark).permit(:name, :location)
   end
